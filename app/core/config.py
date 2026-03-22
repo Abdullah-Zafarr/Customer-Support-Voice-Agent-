e45c183f45b47a8d2440e70787ba29e70a844ee7
abdullahzafar08 <857895.r@gmail.com>
2026-03-22 23:55:34 +0500

    clean: remove old nested folder structure

--- app/core/config.py
+++
@@ -1,9 +0,0 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
-
-class Settings(BaseSettings):
-    GROQ_API_KEY: str = ""
-    DATABASE_URL: str = "sqlite:///./data/appointments.db"
-
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
-
-settings = Settings()
